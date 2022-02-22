b87d92ba01ff082fb4bbbb9d09b9d32f92864e5c
Harry Brown <harrybrown98@gmail.com>
2022-02-21 21:56:05 -0800

    added better form validation to invite form

--- frontend/pages/invite.ts
+++ frontend/pages/invite.ts
@@ -1,4 +1,5 @@
 import "~/frontend/styles/font.css";
+import { isEmail } from "~/frontend/util/email";
 import { millisecondsToStr } from "~/frontend/util/time";
 
 const main = () => {
@@ -12,6 +13,10 @@ const main = () => {
   if (timerText == null) {
     throw new Error("no timer element");
   }
+
+  let form = document.getElementById("signup-form");
+  if (form == null) throw new Error("could not find login form");
+  handleFormActions(form as HTMLFormElement);
 
   let timer: NodeJS.Timer;
   const update = () => {
@@ -33,6 +38,49 @@ const selfDestruct = () => {
   if (msg != null) {
     msg.innerText = "This page has self destructed.";
   }
+};
+
+const handleFormActions = (form: HTMLFormElement) => {
+  let errorContainer = document.createElement("div");
+  form.parentElement?.appendChild(errorContainer);
+
+  form.addEventListener("submit", (ev: SubmitEvent) => {
+    ev.preventDefault();
+    let target = ev.target as HTMLFormElement;
+    let data = new FormData(form as HTMLFormElement);
+    let body = {
+      username: data.get("username") as string,
+      email: data.get("email") as string,
+      password: data.get("password") as string,
+    };
+    if (!isEmail(body.email)) {
+      errorContainer.innerText = "Error: invalid email";
+      return;
+    }
+    if (body.username.length == 0) {
+      errorContainer.innerText = "Error: no username";
+      return;
+    }
+    if (body.password.length < 5) {
+      errorContainer.innerText =
+        "Error: password must be more that 5 characters";
+      return;
+    }
+    fetch(target.action, {
+      method: target.method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then(async (res) => {
+      if (!res.ok) {
+        let msg = await res.json();
+        errorContainer.innerText = `Error: ${msg}`;
+        return;
+      }
+      window.location.pathname = "/";
+    });
+  });
 };
 
 main();
