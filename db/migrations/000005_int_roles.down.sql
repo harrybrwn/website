38da12021ae23dfdf04feb2f190281277b838fa2
Harry Brown <harrybrown98@gmail.com>
2022-04-11 17:23:17 -0700

    fixed db migrations to be ready for prod

--- db/migrations/000005_int_roles.down.sql
+++ db/migrations/000005_int_roles.down.sql
@@ -1,2 +1,55 @@
+CREATE TEMP TABLE tmp_roles_enum (
+	id int,
+	name varchar(32)
+);
+INSERT INTO tmp_roles_enum (id, name)
+VALUES
+	(1, 'admin'),
+	(2, 'default'),
+	(3, 'family'),
+	(4, 'tanya');
+
+
+CREATE OR REPLACE FUNCTION int_role_as_str(int_role INT)
+RETURNS VARCHAR(32)
+LANGUAGE plpgsql AS $$
+DECLARE
+  str_role VARCHAR(32);
+BEGIN
+	SELECT name INTO str_role
+	FROM tmp_roles_enum WHERE id = int_role;
+	return str_role;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION int_roles_as_str(int_roles_arr INT[])
+RETURNS VARCHAR(32)[]
+LANGUAGE plpgsql AS $$
+DECLARE
+  e INT;
+  str_roles VARCHAR(32)[];
+BEGIN
+	FOREACH e IN ARRAY int_roles_arr LOOP
+		str_roles := str_roles || int_role_as_str(e);
+	END LOOP;
+	RETURN str_roles;
+END;
+$$;
+
+CREATE TEMP TABLE tmp_user AS SELECT * FROM "user";
+
+UPDATE "user" SET roles = ARRAY[]::INT[];
+
 ALTER TABLE "user"
-ALTER COLUMN roles TYPE INT[];
+ALTER COLUMN roles TYPE VARCHAR(32)[]
+USING (roles::VARCHAR(32)[]);
+
+UPDATE "user" u
+   SET roles = int_roles_as_str(tu.roles)
+  FROM tmp_user tu
+ WHERE u.id = tu.id;
+
+DROP TABLE tmp_roles_enum;
+DROP TABLE tmp_user;
+DROP FUNCTION int_role_as_str;
+DROP FUNCTION int_roles_as_str;
--- db/migrations/000005_int_roles.up.sql
+++ db/migrations/000005_int_roles.up.sql
@@ -1,2 +1,47 @@
+CREATE TEMP TABLE tmp_roles_enum (
+	id INT,
+	name VARCHAR(32)
+);
+INSERT INTO tmp_roles_enum (id, name)
+VALUES
+	(1, 'admin'),
+	(2, 'default'),
+	(3, 'family'),
+	(4, 'tanya');
+
+CREATE OR REPLACE FUNCTION str_role_as_int(str_role VARCHAR(32))
+RETURNS INT
+LANGUAGE plpgsql AS $$
+DECLARE
+  int_role INT;
+BEGIN
+	SELECT id INTO int_role
+	FROM tmp_roles_enum
+	WHERE name = str_role;
+	return int_role;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION str_roles_as_int(str_roles_arr VARCHAR(32)[])
+RETURNS INT[]
+LANGUAGE plpgsql AS $$
+DECLARE
+  e VARCHAR(32);
+  int_roles INT[];
+BEGIN
+	FOREACH e IN ARRAY str_roles_arr LOOP
+		int_roles := int_roles || str_role_as_int(e);
+	END LOOP;
+	RETURN int_roles;
+END;
+$$;
+
+UPDATE "user" SET roles = str_roles_as_int(roles)::VARCHAR(32)[];
+
 ALTER TABLE "user"
-ALTER COLUMN roles TYPE VARCHAR(32)[];
+ALTER COLUMN roles TYPE INT[]
+USING (roles::INT[]);
+
+DROP TABLE tmp_roles_enum;
+DROP FUNCTION str_role_as_int;
+DROP FUNCTION str_roles_as_int;
