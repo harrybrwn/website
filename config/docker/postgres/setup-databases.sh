718ed41f0c13e88cea4d99bd400b57339c6c0d43
Harry Brown <harrybrown98@gmail.com>
2022-06-16 11:13:38 -0700

    improving logging/metrics config

--- config/docker/postgres/setup-databases.sh
+++ config/docker/postgres/setup-databases.sh
@@ -3,14 +3,43 @@
 set -eu
 
 function create_user_and_database() {
-	local database=$1
-	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	local database=$(printf "%s" "$1" | cut -d: -f1)
+	local owner=$(printf "%s" "$1" | cut -d: -f2 -s)
+	if [ -z "${database:-}" ]; then
+		echo "Error: no database name" 1>&2
+		exit 1
+	fi
+	if [ -n "${owner:-}" ]; then
+		echo "Creating database \"$database\" with owner \"$owner\""
+		psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+				CREATE DATABASE "$database" WITH OWNER "$owner";
+		EOSQL
+	else
+		echo "Creating database \"$database\""
+		psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+				CREATE DATABASE "$database";
+		EOSQL
+	fi
+}
+
+function create_user() {
+	local user=$(printf "%s" "$1" | cut -d: -f1)
+	local pw=$(printf "%s" "$1" | cut -d: -f2)
+	if [ -z "${user:-}" -o -z "${pw:-}" ]; then
+		echo "Error: must have username and password" 1>&2
+		exit 1
+	fi
+	echo "Creating new user \"$user\""
+	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-EOSQL
+		CREATE ROLE "$user" WITH LOGIN PASSWORD '$pw';
 	EOSQL
 }
+
+if [ -n "${POSTGRES_USERS:-}" ]; then
+	for pair in ${POSTGRES_USERS}; do
+		create_user "$pair"
+	done
+fi
 
 if [ -n "${POSTGRES_DATABASES:-}" ]; then
 	echo "Multiple database creation requested: $POSTGRES_DATABASES"
