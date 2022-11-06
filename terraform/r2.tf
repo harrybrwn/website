47d717c9ef4c07f6fce72bac25bbbddd46c8398e
Harry Brown <harrybrown98@gmail.com>
2022-11-06 15:31:27 -0700

    terraform for cloudflare r2

---
+++ terraform/r2.tf
@@ -0,0 +1,23 @@
+locals {
+  r2_domain = "${var.cloudflare_account_id}.r2.cloudflarestorage.com"
+}
+
+provider "aws" {
+  alias      = "cloudflare"
+  access_key = var.r2_access_key_id
+  secret_key = var.r2_secret_access_key
+  region     = "auto"
+  endpoints {
+    s3 = "https://${local.r2_domain}"
+  }
+  skip_credentials_validation = true
+  skip_region_validation      = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms = true
+  s3_use_path_style           = true
+}
+
+resource "aws_s3_bucket" "registry-bucket" {
+  provider           = aws.cloudflare
+  bucket             = "container-registry"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,16 @@ variable "cloudflare_account_id" {
   type = string
 }
 
+variable "r2_access_key_id" {
+  description = "Cloudflare R2 accesss key ID."
+  type = string
+}
+
+variable "r2_secret_access_key" {
+  description = "Cloudflare R2 secret accesss key."
+  type = string
+}
+
 variable "gateway_ip" {
   description = "IP address of the main gateway."
   type        = string
