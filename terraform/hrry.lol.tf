6064391b2eff3b57c81bbccbe21577cf13536312
Harry Brown <harrybrown98@gmail.com>
2023-01-01 11:42:53 -0800

    added minecraft server config

--- terraform/hrry.lol.tf
+++ terraform/hrry.lol.tf
@@ -1,5 +1,14 @@
 resource "cloudflare_record" "minecraft_server" {
   name    = "mc"
+  value   = var.gateway_ip
+  type    = "A"
+  proxied = false
+  ttl     = 120 # seconds
+  zone_id = local.zones.hrry_lol
+}
+
+resource "cloudflare_record" "minecraft_server_local" {
+  name    = "local.mc"
   value   = "10.0.0.68"
   type    = "A"
   proxied = false
