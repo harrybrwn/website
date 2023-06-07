0dddeb8ae940295eebc96a2c1d39eec83c188606
Harry Brown <harrybrown98@gmail.com>
2023-06-06 23:15:34 -0600

    moving stuff around

---
+++ pkg/app/auth.go
@@ -0,0 +1,403 @@
+package app
+
+import (
+	"encoding/hex"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
+	hydra "github.com/ory/hydra-client-go"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"harrybrown.com/pkg/auth"
+	"harrybrown.com/pkg/log"
+)
+
+const (
+	tokenKey = "_token"
+	//maxCookieAge = 2147483647
+)
+
+func NewTokenConfig() auth.TokenConfig {
+	hexseed, hasSeed := os.LookupEnv("JWT_SEED")
+	if hasSeed {
+		logger.Info("creating token config from seed")
+		seed, err := hex.DecodeString(hexseed)
+		if err != nil {
+			panic(errors.Wrap(err, "could not decode private key seed from hex"))
+		}
+		return &tokenConfig{auth.EdDSATokenConfigFromSeed(seed)}
+	}
+	logger.Warn("generating new key pair for token config")
+	return &tokenConfig{auth.GenEdDSATokenConfig()}
+}
+
+type tokenConfig struct{ auth.TokenConfig }
+
+func (tc *tokenConfig) GetToken(r *http.Request) (string, error) {
+	c, err := r.Cookie(tokenKey)
+	if err != nil {
+		return auth.GetBearerToken(r)
+	}
+	return c.Value, nil
+}
+
+type TokenService struct {
+	Tokens       auth.TokenStore
+	Users        UserStore
+	Config       auth.TokenConfig
+	HydraAdmin   hydra.AdminApi
+	CookieDomain string
+}
+
+func (ts *TokenService) Login(c echo.Context) error {
+	var (
+		err  error
+		body struct {
+			Login
+			LoginChallenge string `json:"login_challenge"`
+			Remember       bool   `json:"remember"`
+		}
+		req    = c.Request()
+		ctx    = req.Context()
+		logger = log.FromContext(ctx)
+		binder echo.DefaultBinder
+	)
+
+	logger.Info("starting login request")
+	switch err = binder.BindBody(c, &body); err {
+	case nil:
+		break
+	case echo.ErrUnsupportedMediaType:
+		logger.WithField("content-type", req.Header.Get("Content-Type")).Error("unsupported content type")
+		return err
+	default:
+		err = errors.Wrap(err, "failed to bind user data")
+		return echo.ErrInternalServerError.SetInternal(err)
+	}
+
+	// Login flow
+	var (
+		u      *User
+		claims = auth.GetClaims(c)
+	)
+	if claims == nil {
+		u, err = ts.Users.Login(ctx, &body.Login)
+	} else {
+		u, err = ts.Users.Get(ctx, claims.UUID)
+	}
+	if err != nil {
+		return echo.ErrUnauthorized.SetInternal(errors.Wrap(err, "failed to login"))
+	}
+	logger = logger.WithFields(logrus.Fields{
+		"username": u.Username,
+		"email":    u.Email,
+		"user_id":  u.UUID,
+	})
+
+	logger.Info("handling login_challenge")
+	var redirectTo string
+	if len(body.LoginChallenge) > 0 {
+		r, hydraResp, err := ts.HydraAdmin.AcceptLoginRequest(ctx).
+			LoginChallenge(body.LoginChallenge).
+			AcceptLoginRequest(hydra.AcceptLoginRequest{
+				Subject:  u.Email,
+				Remember: hydra.PtrBool(true),
+				Context: map[string]any{
+					"email":    u.Email,
+					"uuid":     u.UUID.String(),
+					"username": u.Username,
+					"roles":    u.Roles,
+				},
+			}).
+			Execute()
+		if err != nil {
+			logger.WithError(err).Error("failed to accept login request")
+			return &echo.HTTPError{
+				Code:     hydraResp.StatusCode,
+				Message:  http.StatusText(hydraResp.StatusCode),
+				Internal: err,
+			}
+		}
+		defer hydraResp.Body.Close()
+		redirectTo = r.GetRedirectTo()
+		logger.Infof("redirecting to %s", redirectTo)
+	} else {
+		logger.Warn("did not get login challenge")
+	}
+
+	if claims == nil {
+		claims = u.NewClaims()
+	}
+	// Generate both a new access token and refresh token.
+	resp, err := auth.NewTokenResponse(ts.Config, claims)
+	if err != nil {
+		return echo.ErrInternalServerError.SetInternal(
+			errors.Wrap(err, "could not create token response"))
+	}
+	err = ts.Tokens.Set(ctx, u.ID, resp.RefreshToken)
+	if err != nil {
+		return echo.ErrInternalServerError.SetInternal(err)
+	}
+	c.Set(string(auth.ClaimsContextKey), claims)
+	ts.setTokenCookie(c.Response(), resp, claims)
+	return c.JSON(200, map[string]any{
+		"redirect_to": redirectTo,
+	})
+}
+
+func ConsentHandler(admin hydra.AdminApi, users UserStore) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var (
+			body struct {
+				Challenge string `json:"consent_challenge" query:"consent_challenge"`
+			}
+			ctx    = c.Request().Context()
+			logger = log.FromContext(ctx)
+		)
+		err := c.Bind(&body)
+		if err != nil {
+			return err
+		}
+		claims := auth.GetClaims(c)
+		if claims == nil {
+			return echo.ErrUnauthorized
+		}
+		logger = logger.WithFields(log.Fields{
+			"id":      claims.ID,
+			"uuid":    claims.UUID,
+			"subject": claims.Subject,
+		})
+		if body.Challenge == "" {
+			return echo.ErrBadRequest
+		}
+		u, err := users.Get(ctx, claims.UUID)
+		if err != nil {
+			return echo.ErrUnauthorized.SetInternal(err)
+		}
+		cr, hydraRes, err := admin.GetConsentRequest(ctx).ConsentChallenge(body.Challenge).Execute()
+		if err != nil {
+			logger.WithError(err).Error("failed to fetch consent request")
+			return &echo.HTTPError{Code: hydraRes.StatusCode, Internal: err}
+		}
+		defer hydraRes.Body.Close()
+		logger.WithFields(log.Fields{
+			"skip":             *cr.Skip,
+			"client.name":      cr.Client.ClientName,
+			"client.client_id": cr.Client.ClientId,
+		}).Info("accepting consent request")
+		r, hydraConsentResp, err := admin.AcceptConsentRequest(ctx).
+			ConsentChallenge(body.Challenge).
+			AcceptConsentRequest(hydra.AcceptConsentRequest{
+				GrantAccessTokenAudience: cr.RequestedAccessTokenAudience,
+				GrantScope:               cr.RequestedScope,
+				Remember:                 hydra.PtrBool(true),
+				Session: &hydra.ConsentRequestSession{
+					AccessToken: nil,
+					IdToken: map[string]any{
+						"email":   u.Email,
+						"uuid":    u.UUID.String(),
+						"roles":   u.Roles,
+						"name":    u.Username,
+						"picture": "https://hrry.me/favicon.ico", // needed by some services
+					},
+				},
+			}).
+			Execute()
+		if err != nil {
+			logger.Error("failed to accept consent request")
+			return echo.ErrInternalServerError
+		}
+		defer hydraConsentResp.Body.Close()
+		return c.JSON(200, map[string]any{"redirect_to": r.RedirectTo})
+	}
+}
+
+func (ts *TokenService) Token(c echo.Context) error {
+	var (
+		err  error
+		body struct {
+			Login
+			LoginChallenge string `json:"login_challenge"`
+			Remember       bool   `json:"remember"`
+		}
+		req    = c.Request()
+		ctx    = req.Context()
+		logger = log.FromContext(ctx)
+	)
+	switch err = c.Bind(&body); err {
+	case nil:
+		break
+	case echo.ErrUnsupportedMediaType:
+		return err
+	default:
+		err = errors.Wrap(err, "failed to bind user data")
+		return echo.ErrInternalServerError.SetInternal(err)
+	}
+	logger = logger.WithFields(logrus.Fields{
+		"username": body.Username,
+		"email":    body.Email,
+	})
+	setCookie, err := ts.parserCookieQuery(req)
+	if err != nil {
+		return err
+	}
+	logger.Info("getting token")
+	u, err := ts.Users.Login(ctx, &body.Login)
+	if err != nil {
+		return echo.ErrNotFound.SetInternal(errors.Wrap(err, "failed to login"))
+	}
+
+	if len(body.LoginChallenge) > 0 {
+		_, acceptLoginResp, err := ts.HydraAdmin.AcceptLoginRequest(ctx).
+			LoginChallenge(body.LoginChallenge).
+			Execute()
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+		defer acceptLoginResp.Body.Close()
+	}
+	claims := u.NewClaims()
+	// Generate both a new access token and refresh token.
+	resp, err := auth.NewTokenResponse(ts.Config, claims)
+	if err != nil {
+		return echo.ErrInternalServerError.SetInternal(
+			errors.Wrap(err, "could not create token response"))
+	}
+	err = ts.Tokens.Set(ctx, u.ID, resp.RefreshToken)
+	if err != nil {
+		return echo.ErrInternalServerError.SetInternal(err)
+	}
+	c.Set(string(auth.ClaimsContextKey), claims)
+	if setCookie {
+		ts.setTokenCookie(c.Response(), resp, claims)
+	}
+	return c.JSON(200, resp)
+}
+
+type RefreshTokenReq struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+func (ts *TokenService) Refresh(c echo.Context) error {
+	var (
+		err      error
+		req      = c.Request()
+		ctx      = req.Context()
+		logger   = log.FromContext(ctx)
+		tokenReq RefreshTokenReq
+	)
+	err = c.Bind(&tokenReq)
+	if err != nil {
+		return echo.ErrBadRequest.SetInternal(err)
+	}
+	setCookie, err := ts.parserCookieQuery(req)
+	if err != nil {
+		return err
+	}
+
+	refreshClaims, err := auth.ValidateRefreshToken(tokenReq.RefreshToken, ts.keyfunc)
+	if err != nil {
+		return echo.ErrBadRequest.SetInternal(err)
+	}
+
+	stored, err := ts.Tokens.Get(ctx, refreshClaims.ID)
+	if err != nil {
+		return echo.ErrUnauthorized.SetInternal(err)
+	}
+	if tokenReq.RefreshToken != stored {
+		logger.Warn("refresh token did not match up with token in storage")
+		return echo.ErrUnauthorized
+	}
+
+	now := time.Now()
+	claims := auth.Claims{
+		ID:    refreshClaims.ID,
+		UUID:  refreshClaims.UUID,
+		Roles: refreshClaims.Roles,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Audience:  []string{auth.TokenAudience},
+			Issuer:    auth.Issuer,
+			NotBefore: jwt.NewNumericDate(now),
+		},
+	}
+	// Only generate a new access token, the client should still have the refresh token.
+	resp, err := auth.NewAccessToken(ts.Config, &claims)
+	if err != nil {
+		return echo.ErrInternalServerError.SetInternal(err)
+	}
+	// Send back the same refresh token they sent
+	resp.RefreshToken = tokenReq.RefreshToken
+
+	c.Set(string(auth.ClaimsContextKey), claims)
+	if setCookie {
+		ts.setTokenCookie(c.Response(), resp, &claims)
+	}
+	return c.JSON(200, resp)
+}
+
+func (ts *TokenService) parserCookieQuery(req *http.Request) (bool, error) {
+	var (
+		set         bool
+		err         error
+		cookieQuery = req.URL.Query().Get("cookie")
+	)
+	if len(cookieQuery) > 0 {
+		set, err = strconv.ParseBool(cookieQuery)
+		if err != nil {
+			return false, echo.ErrBadRequest.SetInternal(err)
+		}
+	} else {
+		set = false
+	}
+	return set, nil
+}
+
+func (ts *TokenService) setTokenCookie(response http.ResponseWriter, token *auth.TokenResponse, claims *auth.Claims) {
+	http.SetCookie(response, &http.Cookie{
+		Name:     tokenKey,
+		Value:    token.Token,
+		Expires:  claims.ExpiresAt.Time,
+		Path:     "/",
+		SameSite: http.SameSiteNoneMode,
+		Domain:   ts.CookieDomain,
+		Secure:   true,
+	})
+}
+
+func (ts *TokenService) Revoke(c echo.Context) error {
+	var (
+		ctx = c.Request().Context()
+		req RefreshTokenReq
+	)
+	err := c.Bind(&req)
+	if err != nil {
+		return err
+	}
+	claims := auth.GetClaims(c)
+	if claims == nil {
+		return echo.ErrBadRequest
+	}
+	token, err := ts.Tokens.Get(ctx, claims.ID)
+	if err != nil {
+		return err
+	}
+	if req.RefreshToken != token {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
+			Message: "incorrect refresh token",
+		}
+	}
+	err = ts.Tokens.Del(c.Request().Context(), claims.ID)
+	if err != nil {
+		return echo.ErrInternalServerError
+	}
+	return nil
+}
+
+func (ts *TokenService) keyfunc(*jwt.Token) (interface{}, error) {
+	return ts.Config.Public(), nil
+}
---
+++ pkg/app/auth_test.go
@@ -0,0 +1,295 @@
+package app
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	mathrand "math/rand"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/matryer/is"
+	"golang.org/x/crypto/bcrypt"
+	"harrybrown.com/pkg/auth"
+	"harrybrown.com/pkg/internal/mocks/mockdb"
+	"harrybrown.com/pkg/internal/mockutil"
+)
+
+func TestTokenHandler(t *testing.T) {
+	defer silent()()
+	var (
+		ctx        = context.Background()
+		loginQuery = func(db *mockdb.MockDB) *gomock.Call {
+			return db.EXPECT().QueryContext(
+				ctx,
+				mockutil.HasPrefix(selectQueryHead),
+				gomock.AssignableToTypeOf(""),
+			)
+		}
+		loginScan = func(rows *mockdb.MockRows) *gomock.Call {
+			return rows.EXPECT().Scan(
+				gomock.AssignableToTypeOf(intptr),
+				gomock.AssignableToTypeOf(&uuid.UUID{}),
+				gomock.AssignableToTypeOf(strptr),   // username
+				gomock.AssignableToTypeOf(strptr),   // email
+				gomock.AssignableToTypeOf(bytesptr), // password hash
+				gomock.AssignableToTypeOf(strptr),   // totp key
+				gomock.Any(),                        // roles
+				gomock.AssignableToTypeOf(&time.Time{}),
+				gomock.AssignableToTypeOf(&time.Time{}),
+			)
+		}
+	)
+
+	type table struct {
+		cfg   auth.TokenConfig
+		body  map[string]interface{}
+		errs  []error
+		query url.Values
+		prep  func(db *mockdb.MockDB, rows *mockdb.MockRows)
+	}
+
+	for i, tt := range []table{
+		{
+			cfg:  auth.GenEdDSATokenConfig(),
+			body: nil,
+			errs: []error{ErrEmptyPassword, echo.ErrNotFound},
+		},
+		{
+			cfg:  auth.GenEdDSATokenConfig(),
+			body: map[string]interface{}{"password": "1234"},
+			errs: []error{ErrUserNotFound, echo.ErrNotFound},
+		},
+		{
+			cfg:  auth.GenerateECDSATokenConfig(),
+			body: map[string]interface{}{"email": "joe@joe.com", "password": "im the real joe"},
+			errs: []error{sql.ErrConnDone, echo.ErrNotFound},
+			prep: func(db *mockdb.MockDB, rows *mockdb.MockRows) {
+				loginQuery(db).Return(rows, nil)
+				rows.EXPECT().Next().Return(true)
+				loginScan(rows).Return(sql.ErrConnDone)
+				rows.EXPECT().Close().Return(nil)
+			},
+		},
+		{
+			cfg:  auth.GenerateECDSATokenConfig(),
+			body: map[string]interface{}{"email": "joe@joe.com", "password": "im the real joe"},
+			errs: []error{ErrUserNotFound, echo.ErrNotFound},
+			prep: func(db *mockdb.MockDB, rows *mockdb.MockRows) {
+				loginQuery(db).Return(rows, nil)
+				rows.EXPECT().Next().Return(false)
+				rows.EXPECT().Err().Return(ErrUserNotFound)
+				rows.EXPECT().Close().Return(nil)
+			},
+		},
+		{
+			cfg:  auth.GenerateECDSATokenConfig(),
+			body: map[string]interface{}{"email": "joe@joe.com", "password": "im the real joe"},
+			errs: []error{sql.ErrNoRows, echo.ErrNotFound},
+			prep: func(db *mockdb.MockDB, rows *mockdb.MockRows) {
+				loginQuery(db).Return(rows, sql.ErrNoRows)
+			},
+		},
+		{
+			cfg:  auth.GenEdDSATokenConfig(),
+			body: map[string]interface{}{"username": "tester", "password": "asdfasdf"},
+			errs: []error{sql.ErrNoRows, echo.ErrNotFound},
+			prep: func(db *mockdb.MockDB, rows *mockdb.MockRows) {
+				loginQuery(db).Times(1).Return(rows, nil)
+				rows.EXPECT().Next().Return(false)
+				rows.EXPECT().Err().Return(nil)
+				rows.EXPECT().Close().Return(nil)
+			},
+		},
+		{
+			cfg:  auth.GenerateECDSATokenConfig(),
+			body: map[string]interface{}{"email": "joe@joe.com", "password": "im the real joe"},
+			errs: []error{ErrWrongPassword, echo.ErrNotFound},
+			prep: func(db *mockdb.MockDB, rows *mockdb.MockRows) {
+				loginQuery(db).Return(rows, nil)
+				rows.EXPECT().Next().Return(true)
+				loginScan(rows).Return(nil)
+				rows.EXPECT().Close().Return(nil)
+			},
+		},
+		{
+			cfg:   auth.GenerateECDSATokenConfig(),
+			body:  map[string]interface{}{"username": "tester", "password": "asdfasdf"},
+			query: url.Values{"cookie": {"true"}},
+			prep: func(db *mockdb.MockDB, rows *mockdb.MockRows) {
+				loginQuery(db).Times(1).Return(rows, nil)
+				rows.EXPECT().Next().Return(true)
+				loginScan(rows).Times(1).Do(func(v ...interface{}) {
+					pw := v[4].(*[]byte)
+					var e error
+					*pw, e = bcrypt.GenerateFromPassword([]byte("asdfasdf"), hashCost)
+					if e != nil {
+						t.Fatal(e)
+					}
+				}).Return(nil)
+				rows.EXPECT().Close().Return(nil)
+			},
+		},
+		{
+			cfg:   auth.GenEdDSATokenConfig(),
+			errs:  []error{echo.ErrBadRequest},
+			query: url.Values{"cookie": {"notaboolean"}},
+		},
+	} {
+		if tt.prep == nil {
+			tt.prep = func(*mockdb.MockDB, *mockdb.MockRows) {}
+		}
+		t.Run(fmt.Sprintf("TestTokenHandler_%d", i), func(t *testing.T) {
+			var (
+				is   = is.New(t)
+				e    = echo.New()
+				ctrl = gomock.NewController(t)
+				db   = mockdb.NewMockDB(ctrl)
+				rows = mockdb.NewMockRows(ctrl)
+				rec  = httptest.NewRecorder()
+				req  = httptest.NewRequest("POST", "/", body(tt.body)).WithContext(ctx)
+			)
+			defer ctrl.Finish()
+			req.Header.Set(echo.HeaderContentType, "application/json")
+			req.URL.RawQuery = tt.query.Encode()
+
+			service := TokenService{
+				Config: tt.cfg,
+				Users:  NewUserStore(db),
+				Tokens: auth.NewInMemoryTokenStore(time.Minute),
+			}
+			tt.prep(db, rows)
+			c := e.NewContext(req, rec)
+			err := service.Token(c)
+			checkErrs(t, tt.errs, err)
+
+			if len(tt.errs) > 0 {
+				return
+			}
+
+			resp := rec.Result()
+			defer resp.Body.Close()
+			var (
+				tok     *auth.TokenResponse
+				cookies = resp.Cookies()
+			)
+			is.NoErr(json.NewDecoder(resp.Body).Decode(&tok))
+			is.True(len(tok.Token) > 1)
+			is.True(len(tok.RefreshToken) > 1)
+			is.Equal(1, len(cookies))
+			is.Equal("/", cookies[0].Path)
+			is.Equal(cookies[0].Value, tok.Token)
+			is.Equal(cookies[0].Expires, tok.Expires.Time.UTC())
+			claims := auth.GetClaims(c)
+			is.True(claims != nil)
+		})
+	}
+}
+
+func TestRefreshTokenHandler_Err(t *testing.T) {
+	type table struct {
+		prep func(claims *auth.Claims, v url.Values)
+	}
+	for i, tt := range []table{
+		{prep: func(claims *auth.Claims, v url.Values) { claims.Audience = []string{"not correct aud"} }},
+		{prep: func(claims *auth.Claims, v url.Values) {
+			n := time.Now()
+			claims.ExpiresAt = jwt.NewNumericDate(time.Date(
+				n.Year(), n.Month(), n.Day(), n.Hour()-5,
+				n.Minute(), n.Second(), n.Nanosecond(), n.Location(),
+			))
+		}},
+		{prep: func(claims *auth.Claims, v url.Values) {
+			v.Add("cookie", "not-a-boolean")
+		}},
+	} {
+		t.Run(fmt.Sprintf("%s_%d", t.Name(), i), func(t *testing.T) {
+			is := is.New(t)
+			tokenCfg := auth.GenEdDSATokenConfig()
+			claims := &auth.Claims{
+				ID:   mathrand.Int(),
+				UUID: uuid.New(),
+				RegisteredClaims: jwt.RegisteredClaims{
+					IssuedAt:  jwt.NewNumericDate(time.Now()),
+					ExpiresAt: jwt.NewNumericDate(time.Now().Add(auth.Expiration)),
+					Audience:  []string{"refresh"},
+					Issuer:    auth.Issuer,
+				},
+			}
+			params := url.Values{}
+			userClaims := *claims
+			userClaims.Audience = []string{auth.TokenAudience}
+			if tt.prep != nil {
+				tt.prep(claims, params)
+			}
+			refreshToken, err := jwt.NewWithClaims(tokenCfg.Type(), claims).SignedString(tokenCfg.Private())
+			is.NoErr(err)
+			store := auth.NewInMemoryTokenStore(time.Minute)
+			is.NoErr(store.Set(context.Background(), claims.ID, refreshToken))
+			e := echo.New()
+			rec := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/api/refresh", body(map[string]interface{}{"refresh_token": refreshToken}))
+			req.URL.RawQuery = params.Encode()
+			req.Header.Set("Content-Type", "application/json")
+			c := e.NewContext(req, rec)
+			service := TokenService{
+				Tokens: store,
+				Config: tokenCfg,
+			}
+			err = service.Refresh(c)
+			if err == nil {
+				t.Fatal("expected an error got nil")
+			}
+		})
+	}
+}
+
+func TestRefreshTokenHandler(t *testing.T) {
+	is := is.New(t)
+	tokenCfg := auth.GenEdDSATokenConfig()
+	claims := &auth.Claims{
+		ID:   mathrand.Int(),
+		UUID: uuid.New(),
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(auth.Expiration)),
+			Audience:  []string{"refresh"},
+			Issuer:    auth.Issuer,
+		},
+	}
+	refreshToken, err := jwt.NewWithClaims(tokenCfg.Type(), claims).SignedString(tokenCfg.Private())
+	is.NoErr(err)
+	store := auth.NewInMemoryTokenStore(time.Minute)
+	is.NoErr(store.Set(context.Background(), claims.ID, refreshToken))
+	e := echo.New()
+	req := httptest.NewRequest(
+		"POST", "/api/refresh?cookie=true",
+		body(map[string]interface{}{"refresh_token": refreshToken}),
+	)
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	service := TokenService{
+		Config: tokenCfg,
+		Tokens: store,
+	}
+	err = service.Refresh(c)
+	is.NoErr(err)
+	res := rec.Result()
+	defer res.Body.Close()
+	is.Equal(res.StatusCode, 200)
+	is.Equal(1, len(res.Cookies()))
+	cookie := res.Cookies()[0]
+	is.Equal(cookie.Name, tokenKey)
+	is.Equal(cookie.Path, "/")
+	var tok auth.TokenResponse
+	is.NoErr(json.NewDecoder(res.Body).Decode(&tok))
+	is.True(1 < len(tok.Token))
+}
